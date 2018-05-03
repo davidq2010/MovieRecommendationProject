6e2016367756f7bfaabb2bd6ea7010edac4ebd52
Tram Nguyen <tramnguyen@Trams-MacBook-Pro.local>
2018-05-03 17:32:52 -0400

    Split data

--- testLogisticRegression.py
+++ testLogisticRegression.py
@@ -1,6 +1,8 @@
 #import sklearn.linear_model.LogisticRegression
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
 
 def readDataFromFeatureFile(_file):
   '''
@@ -49,3 +51,9 @@ Y = readDataFromTargetFile('y_train.txt')
 
 print(X)
 print(Y)
+
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2)
+print(x_train.shape, y_train.shape)
+print(x_test.shape, y_test.shape)
+
+
